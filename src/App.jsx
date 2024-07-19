9267f55e82ebff7f6f7d8d1b49a9bb60a869f278
Andoni Christou <andonilc71@gmail.com>
2024-07-18 22:01:28 -0400

    figured out some api, effect, state, and rendering functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,50 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [imgUrls, setImgUrls] = useState([])
+
+  
 
   useEffect(() => {
+    let isMounted = true;
+    console.log("effect running")
     async function fetchData() {
       try {
-        const response = await fetch('');
+        const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${import.meta.env.VITE_GIPHY_API_KEY}&q=Seinfeld&limit=3&offset=0&rating=g&lang=en&bundle=messaging_non_clips`);
+        if (!response.ok) {
+          throw new Error("Network response was not ok")
+        }
+        const json = await response.json()
+        console.log(json)
+        
+        if (isMounted) {
+          setImgUrls(prevImgUrls => 
+            json.data.map((item) => item.images.original.url)
+          );
+          //console.log(imgUrls)
+        }
+        
+      } catch (error) {
+        console.error(error)
       }
     }
-  });
+
+    fetchData();
+
+    return () => {
+      isMounted = false;
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(imgUrls)
+  }, [imgUrls])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      {imgUrls.map(imgUrl => 
+        <img src={imgUrl}/>
+      )}
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
